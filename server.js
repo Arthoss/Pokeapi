3719f091f7da286f8a5cbf0bdac9f3b64501ddb2
Daniel <daniellopezcastiblanco13@gmail.com>
2026-05-22 20:58:23 -0500

    fix: mover db.js a src y agregar auth.js

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 const http = require('http');
 const { handleRequest } = require('./src/router');
- 
+
 const PORT = process.env.PORT || 3000;
- 
+
 const server = http.createServer(handleRequest);
- 
+
 server.listen(PORT, () => {
-  console.log(`\n🎌 Anime API corriendo en http://localhost:${PORT}`);
+  console.log(`\n🎌 Anime API v2.0 corriendo en http://localhost:${PORT}`);
   console.log(`📚 Documentación Swagger: http://localhost:${PORT}/docs`);
-  console.log(`🔗 Endpoints disponibles:`);
-  console.log(`   GET http://localhost:${PORT}/api/animes`);
-  console.log(`   GET http://localhost:${PORT}/api/personajes`);
-  console.log(`   GET http://localhost:${PORT}/api/personajes/:nombre`);
-  console.log(`   GET http://localhost:${PORT}/api/personajes/anime/:anime`);
-  console.log(`   GET http://localhost:${PORT}/api/personajes/:nombre/imagenes\n`);
 });
- 
